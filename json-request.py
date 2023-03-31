66829576ec6b00e69cf669380bb8a929e3e0697a
wavilla <wavilla@ucsc.edu>
2023-03-30 21:00:29 -0700

    added json request file

---
+++ json-request.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+def request_tts_conversion(input_text): #requests for base64 audio conversion of argument
+    voice = "en_us_001"
+    ENDPOINT = "https://tiktok-tts.weilnet.workers.dev"
+
+    url = f"{ENDPOINT}/api/generation"
+    headers = {'Content-Type': 'application/json'}
+    data = {'text': input_text, 'voice': voice}
+
+    response = (requests.post(url, headers=headers, data=json.dumps(data))).json()
+
+    return response["data"]
