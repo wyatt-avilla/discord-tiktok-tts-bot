d13545635b92225759eb30c04f000b44cd1e6e9a
wyatt-avilla <wyattmurphy1@gmail.com>
2023-04-01 20:26:39 -0700

    pruned unwanted commands

--- bot.py
+++ bot.py
@@ -20,19 +20,18 @@ async def on_ready():
     except Exception as err:
         print(f"unable to sync properly with exception: {err}")
 
-
-@bot.tree.command(name = "affirm", description = "<3")
-async def affirm(interaction: discord.Interaction):
-    await (interaction.user).send("i love u")
-    await interaction.response.send_message("check ur dms", ephemeral=True)
-
 @bot.tree.command(name = "join", description = "joins ur vc")
 async def join(interaction: discord.Interaction):
-    try:
-        await (interaction.user.voice.channel).connect()
-        await interaction.response.send_message("omw", ephemeral = True)
-    except Exception as err:
-        await interaction.response.send_message(f"unable to join voice with exception: {err}")
+    user_voice_channel = interaction.user.voice
+    if user_voice_channel != None:
+        try:
+            await (user_voice_channel).connect()
+            await interaction.response.send_message("omw", ephemeral = True)
+        except Exception as err:
+            await interaction.response.send_message(f"unable to join voice")
+            print(f"bot was unable to join voice with exception: {err}")
+    else:
+        await interaction.response.send_message("u have to be in a voice call to use this command")
 
 @bot.tree.command(name = "leave", description = "leaves voice")
 async def leave(interaction: discord.Interaction):
@@ -47,34 +46,7 @@ async def leave(interaction: discord.Interaction):
     else:
         await interaction.response.send_message(f"im not in vc", ephemeral = True)
 
-@bot.tree.command(name = "debug", description = "debug tool")
-async def debug(interaction: discord.Interaction):
-    try:
-        await interaction.response.send_message(f"check console")
-        print(f"user: {interaction.user}")
-        print(f"user voice channel: {interaction.user.voice.channel}")
-        print(f"user voice client: {interaction.guild.voice_client}")
-        print(f"user voice state: {interaction.user.voice}")
-        print(f"bot channel: {interaction.guild.voice_channels}")
-        print(f"bot voice client: {discord.utils.get(bot.voice_clients, guild=interaction.guild)}")
-        print(f"current guild id: {interaction.guild_id}")
-        print(f"current guild: {interaction.guild}")
-
-        if (interaction.guild.voice_client) == (discord.utils.get(bot.voice_clients, guild=interaction.guild)):
-            print("bot and user have same voice client")
-
-    except Exception as err:
-        await interaction.response.send_message(f"wow, even the debug has errors... {err}")
-
-@bot.tree.command(name = "sync", description = "syncs commands")
-async def sync(interaction: discord.Interaction):
-    try:
-        await bot.tree.sync(guild=discord.Object(id=1043400553385955350))
-        await interaction.response.send_message(f"synced commands properly")
-    except Exception as err:
-        await interaction.response.send_message(f"unable to sync with exception: {err}")
-
-@bot.tree.command(name = "tts", description = "reads text in the funny tiktok voice", guild=(discord.Object(id=1043400553385955350)))
+@bot.tree.command(name = "tts", description = "reads text in the funny tiktok voice")
 @app_commands.describe(voice = "the voice to be used", text = "text to be converted to to speech")
 @app_commands.choices(voice = [
     Choice(name = "American Woman", value = "en_us_001"),
@@ -137,75 +109,6 @@ async def tts(interaction: discord.Interaction, voice: str, text: str):
         except Exception as err:
             await interaction.response.send_message("something went wrong", ephemeral=True)
             print(f"Exception: {err}")
-
-@bot.tree.command(name = "sing", description = "sings given text", guild=(discord.Object(id=1043400553385955350)))
-@app_commands.describe(voice = "the voice to be used", text = "text to be sung")
-@app_commands.choices(voice = [
-    Choice(name = "Alto Singing", value = "en_female_f08_salut_damour"),
-    Choice(name = "Tenor Singing", value = "en_male_m03_lobby"),
-    Choice(name = "Sunshine Soon", value = "en_male_m03_sunshine_soon"),
-    Choice(name = "Warmy Breeze", value = "en_female_f08_warmy_breeze"),
-    Choice(name = "Glorious", value = "en_female_ht_f08_glorious"),
-    Choice(name = "It Goes Up", value = "en_male_sing_funny_it_goes_up"),
-    Choice(name = "Chipmunk", value = "en_male_m2_xhxs_m03_silly"),
-    Choice(name = "Dramatic", value = "en_female_ht_f08_wonderful_world"),
-])
-async def sing(interaction: discord.Interaction, voice: str, text: str):
-    bot_voice_client = discord.utils.get(bot.voice_clients, guild=interaction.guild)
-
-    if bot_voice_client != None:
-        try:
-            converted_text = request_tts_conversion(text, voice)
-            vc = interaction.guild.voice_client
-            player = discord.FFmpegPCMAudio(source= converted_text)
-
-            vc.play(player)
-
-            await interaction.response.send_message(f"successfully sung: `\"{text}\"`", ephemeral = True)
-
-        except Exception as err:
-            await interaction.response.send_message("something went wrong", ephemeral=True)
-            print(f"Exception: {err}")
-    else: 
-        try:
-            await (interaction.user.voice.channel).connect()
-        except Exception as err:
-            await interaction.response.send_message(f"attempt to join voice failed", ephemeral= True)
-            print(err)
-        try:
-            converted_text = request_tts_conversion(text, voice)
-            vc = interaction.guild.voice_client
-            player = discord.FFmpegPCMAudio(source= converted_text)
-
-            vc.play(player)
-
-            await interaction.response.send_message(f"successfully sung: `\"{text}\"`", ephemeral= True)
-
-        except Exception as err:
-            await interaction.response.send_message("something went wrong", ephemeral=True)
-            print(f"Exception: {err}")
-
-
-
-
-
-
-
-
-@bot.tree.command(name = "test", guild=(discord.Object(id=1043400553385955350)))
-async def test(interaction: discord.Interaction, param: str):
-    #print(f"The state of this interaction is: {interaction.is_expired()}")
-    print(f"this interaction expires at: {interaction.expires_at}")
-
-    if param == "1":
-        await interaction.response.send_message("send_message")
-    elif param == "2":
-        await interaction.followup.send("followup message")
-    elif param == "3":
-        await interaction.delete_original_response()
-    elif param == "4":
-        await interaction.response.defer()
-        await interaction.followup("followed up here !")
 
 
 if __name__ == "__main__":
